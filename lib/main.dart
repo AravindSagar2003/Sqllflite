50e6b569b7033ea099e6602a72697c5bd8e2b148
Aravind <AravindSagar2003@gmail.com>
2024-08-21 11:42:21 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(appBar: AppBar(backgroundColor: Colors.yellow,),),
+
+
+
+
+
+      home: Scaffold(appBar: AppBar(backgroundColor: Colors.black,),),
       
       ); 
     
